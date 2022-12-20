ee49a6b55f429e31c5fc063ce092a22d920904a5
baize-live <1921676794@qq.com>
2022-12-20 11:33:42 +0800

    完善路由器程序

--- Capture/lab5.cpp
+++ Capture/lab5.cpp
@@ -4,6 +4,7 @@
 #include "lab.h"
 #include <vector>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -16,6 +17,7 @@ struct Routing_Table_Entry {
     DWORD NextIP;
 };
 
+int num = 0;
 MAC_Address localMAC;
 Routing_Table_Entry localIP[2];
 map<DWORD, MAC_Address> ARP_Table;
@@ -72,25 +74,80 @@ int main() {
         exit(1);
     }
 
+    Routing_Table.push_back({localIP[0].DesIP & localIP[0].Mask, localIP[0].Mask, inet_addr("0.0.0.0")});
+    Routing_Table.push_back({localIP[1].DesIP & localIP[1].Mask, localIP[1].Mask, inet_addr("0.0.0.0")});
+    cout << "添加(add)/删除(del)/显示(print)路由表项.(输入 break 跳出.) " << endl;
     string str;
-    cout << "请添加路由表项.(输入 break 跳出.) " << endl;
     DWORD DesIP, Mask, NextIP;
     while (true) {
-        cout << "请输入目的IP地址:";
+        cout << "请输入操作: ";
         cin >> str;
         if (str == "break") {
             break;
+        } else if (str == "add") {
+            cout << "请输入目的IP地址:";
+            cin >> str;
+            DesIP = inet_addr(str.c_str());
+            cout << "请输入子网掩码:";
+            cin >> str;
+            Mask = inet_addr(str.c_str());
+            cout << "请输入下一跳IP地址:";
+            cin >> str;
+            NextIP = inet_addr(str.c_str());
+
+            for (auto it= Routing_Table.begin(); it != Routing_Table.end(); it++) {
+                if (it->DesIP == DesIP && it->Mask == Mask) {
+                    it->NextIP = NextIP;
+                    cout << "路由表项已存在, 已更新." << endl;
+                    break;
+                }
+                if(it->Mask < Mask) {
+                    Routing_Table.insert(it, {DesIP, Mask, NextIP});
+                    cout << "路由表项已添加." << endl;
+                    break;
+                }
+                if (it == Routing_Table.end() - 1) {
+                    Routing_Table.push_back({DesIP, Mask, NextIP});
+                    cout << "路由表项已添加." << endl;
+                    break;
+                }
+            }
+        } else if (str == "del") {
+            cout << "请输入目的IP地址:";
+            cin >> str;
+            DesIP = inet_addr(str.c_str());
+            cout << "请输入子网掩码:";
+            cin >> str;
+            Mask = inet_addr(str.c_str());
+
+            if (DesIP == localIP[0].NextIP || DesIP == localIP[1].NextIP) {
+                cout << "无法删除默认路由." << endl;
+                continue;
+            }
+            for (auto it = Routing_Table.begin(); it != Routing_Table.end(); it++) {
+                if (it->DesIP == DesIP && it->Mask == Mask) {
+                    Routing_Table.erase(it);
+                    cout << "删除路由成功." << endl;
+                    break;
+                }
+            }
+        } else if (str == "print") {
+            for (auto &item: Routing_Table) {
+                cout << "目的IP: ";
+                print_ip(item.DesIP);
+                cout << " 子网掩码: ";
+                print_ip(item.Mask);
+                cout << " 下一跳IP: ";
+                print_ip(item.NextIP);
+                cout << endl;
+            }
+        } else {
+            cout << "输入错误" << endl;
         }
-        DesIP = inet_addr(str.c_str());
-        cout << "请输入子网掩码:";
-        cin >> str;
-        Mask = inet_addr(str.c_str());
-        cout << "请输入下一跳IP地址:";
-        cin >> str;
-        NextIP = inet_addr(str.c_str());
-        Routing_Table.push_back({DesIP, Mask, NextIP});
         cout << endl;
     }
+
+    cout << "开始路由转发..." << endl;
 
     // 捕获字节流
     capture_bytes(ad_handle);
@@ -108,15 +165,12 @@ void capture_bytes(pcap_t *&ad_handle) {
     int res;
     pcap_pkthdr *header;
     const u_char *pkt_data;
-    int pkt_num = 0;
 
     while ((res = pcap_next_ex(ad_handle, &header, &pkt_data)) >= 0) {
 
         if (res == 0) {
             continue;
         }
-
-        cout << "第" << ++pkt_num << "条报文 " << endl;
 
         route_forwarding(ad_handle, (byte *) pkt_data, (int) header->len);
     }
@@ -142,6 +196,7 @@ MAC_Address find_MAC_from_ARP_Table(pcap_t *&ad_handle, DWORD IP, bool &ret) {
 void route_forwarding(pcap_t *&ad_handle, byte *buf, int len) {
     auto *MAC_Frame = (MACFrame_t *) buf;
 
+    // 本机Mac不处理
     if (memcmp(MAC_Frame->MAC_Frame_Head.SrcMAC, localMAC.MAC, 6) == 0) {
         return;
     }
@@ -152,53 +207,49 @@ void route_forwarding(pcap_t *&ad_handle, byte *buf, int len) {
 
     auto *IPv4_Datagram = (IPV4_Datagram_t *) MAC_Frame->data;
 
+    cout << "第" << ++num << "条IP数据报, ";
+
     // 本机IP 不处理
     if (IPv4_Datagram->IPV4_Datagram_Head.DesIP == localIP[0].DesIP ||
-        IPv4_Datagram->IPV4_Datagram_Head.DesIP == localIP[1].DesIP) {
+        IPv4_Datagram->IPV4_Datagram_Head.DesIP == localIP[1].DesIP ||
+        (IPv4_Datagram->IPV4_Datagram_Head.DesIP & localIP[0].Mask) ==
+        (inet_addr("255.255.255.255") & localIP[0].Mask) ||
+        (IPv4_Datagram->IPV4_Datagram_Head.DesIP & localIP[1].Mask) ==
+        (inet_addr("255.255.255.255") & localIP[1].Mask)) {
+        cout << "目的IP为本机IP, 不处理." << endl;
         return;
     }
 
-    // 直接投递
-    if (((IPv4_Datagram->IPV4_Datagram_Head.DesIP & localIP[0].Mask) == localIP[0].NextIP) ||
-        ((IPv4_Datagram->IPV4_Datagram_Head.DesIP & localIP[1].Mask) == localIP[1].NextIP)) {
-        bool ret = false;
-        MAC_Address MAC_address = find_MAC_from_ARP_Table(ad_handle, IPv4_Datagram->IPV4_Datagram_Head.DesIP, ret);
-        if (!ret) {
-            cout << "直接投递异常, 获得MAC失败." << endl;
-            return;
-        }
-        memcpy(MAC_Frame->MAC_Frame_Head.DesMAC, MAC_address.MAC, 6);
-        memcpy(MAC_Frame->MAC_Frame_Head.SrcMAC, localMAC.MAC, 6);
-        cout << "直接投递成功. SrcIP:";
-        print_ip(IPv4_Datagram->IPV4_Datagram_Head.SrcIP);
-        cout << " DesIP:";
-        print_ip(IPv4_Datagram->IPV4_Datagram_Head.DesIP);
-        cout << endl;
-        pcap_sendpacket(ad_handle, buf, len);
-        return;
-    }
+    cout << "SrcIP:";
+    print_ip(IPv4_Datagram->IPV4_Datagram_Head.SrcIP);
+    cout << " DesIP:";
+    print_ip(IPv4_Datagram->IPV4_Datagram_Head.DesIP);
 
-    // 路由转发
     for (auto &item: Routing_Table) {
         // 找到路由表中的目的地址
         if ((IPv4_Datagram->IPV4_Datagram_Head.DesIP & item.Mask) == item.DesIP) {
             bool ret = false;
-            MAC_Address MAC_address = find_MAC_from_ARP_Table(ad_handle, item.NextIP, ret);
+            MAC_Address MAC_address{};
+            if (item.NextIP == 0) {
+                // 直接投递
+                MAC_address = find_MAC_from_ARP_Table(ad_handle, IPv4_Datagram->IPV4_Datagram_Head.DesIP, ret);
+            } else {
+                // 路由转发
+                MAC_address = find_MAC_from_ARP_Table(ad_handle, item.NextIP, ret);
+            }
             if (!ret) {
-                cout << "获得MAC失败, 路由转发异常." << endl;
+                cout << " 获得MAC失败" << endl;
                 return;
             }
             memcpy(MAC_Frame->MAC_Frame_Head.DesMAC, MAC_address.MAC, 6);
             memcpy(MAC_Frame->MAC_Frame_Head.SrcMAC, localMAC.MAC, 6);
-            cout << "路由转发成功. SrcIP:";
-            print_ip(IPv4_Datagram->IPV4_Datagram_Head.SrcIP);
-            cout << " DesIP:";
-            print_ip(IPv4_Datagram->IPV4_Datagram_Head.DesIP);
-            cout << " NextIP:";
-            print_ip(item.NextIP);
+            cout << (item.NextIP == 0 ? " 直接投递. NextIP:" : " 转发成功. NextIP:");
+            print_ip(item.NextIP == 0 ? IPv4_Datagram->IPV4_Datagram_Head.DesIP : item.NextIP);
             cout << endl;
             pcap_sendpacket(ad_handle, buf, len);
             return;
         }
     }
+
+    cout << " 转发失败." << endl;
 }
